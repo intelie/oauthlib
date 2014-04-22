152d111a0034eb9aa81d5722fbb23b444af17d16
Lucas Sampaio <lucas@lsmagalhaes.com>
2014-04-22 19:27:18 -0300

    OIDC-capable pre-configured server created

--- oauthlib/oauth2/rfc6749/endpoints/pre_configured.py
+++ oauthlib/oauth2/rfc6749/endpoints/pre_configured.py
@@ -9,6 +9,7 @@ for consuming and providing OAuth 2.0 RFC6749.
 from __future__ import absolute_import, unicode_literals
 
 from ..tokens import BearerToken
+from ..id_token import id_token_modifier
 from ..grant_types import AuthorizationCodeGrant
 from ..grant_types import ImplicitGrant
 from ..grant_types import ResourceOwnerPasswordCredentialsGrant
@@ -198,3 +199,52 @@ class BackendApplicationServer(TokenEndpoint, ResourceEndpoint,
                 token_types={'Bearer': bearer})
         RevocationEndpoint.__init__(self, request_validator,
                 supported_token_types=['access_token'])
+
+
+class OpenIDConnectServer(AuthorizationEndpoint, TokenEndpoint, ResourceEndpoint,
+        RevocationEndpoint):
+    """An all-in-one endpoint featuring all four major grant types."""
+
+    def __init__(self, request_validator, token_expires_in=None,
+                 token_generator=None, refresh_token_generator=None,
+                 *args, **kwargs):
+        """Construct a new all-grants-in-one server.
+
+        :param request_validator: An implementation of
+                                  oauthlib.oauth2.RequestValidator.
+        :param token_expires_in: An int or a function to generate a token
+                                 expiration offset (in seconds) given a
+                                 oauthlib.common.Request object.
+        :param token_generator: A function to generate a token from a request.
+        :param refresh_token_generator: A function to generate a token from a
+                                        request for the refresh token.
+        :param kwargs: Extra parameters to pass to authorization-,
+                       token-, resource-, and revocation-endpoint constructors.
+        """
+        auth_grant = AuthorizationCodeGrant(request_validator)
+        implicit_grant = ImplicitGrant(request_validator)
+        hybrid_grant = None
+        password_grant = ResourceOwnerPasswordCredentialsGrant(request_validator)
+        credentials_grant = ClientCredentialsGrant(request_validator)
+        refresh_grant = RefreshTokenGrant(request_validator)
+        bearer = BearerToken(request_validator, token_generator,
+                             token_expires_in, refresh_token_generator)
+        bearer.register_token_modifier(id_token_modifier)
+        AuthorizationEndpoint.__init__(self, default_response_type='code',
+                response_types=[
+                    ('code', auth_grant),
+                    ('code token', implicit_grant),
+                    ('code token id_token', hybrid_grant),
+                ],
+                default_token_type=bearer)
+        TokenEndpoint.__init__(self, default_grant_type='authorization_code',
+                grant_types={
+                    'authorization_code': auth_grant,
+                    'password': password_grant,
+                    'client_credentials': credentials_grant,
+                    'refresh_token': refresh_grant,
+                },
+                default_token_type=bearer)
+        ResourceEndpoint.__init__(self, default_token='Bearer',
+                token_types={'Bearer': bearer})
+        RevocationEndpoint.__init__(self, request_validator)
